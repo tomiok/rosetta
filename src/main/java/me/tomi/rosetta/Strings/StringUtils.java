882c6e8d1c9ae41a69692fbd349bbf4e8c91b4c8
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-01-28 01:22:17 -0300

    Add hash utility and strings

---
+++ src/main/java/me/tomi/rosetta/Strings/StringUtils.java
@@ -0,0 +1,6 @@
+package me.tomi.rosetta.Strings;
+
+public final class StringUtils {
+
+  public static final String EMPTY = "";
+}
--- src/main/java/me/tomi/rosetta/hash/HashUtils.java
+++ src/main/java/me/tomi/rosetta/hash/HashUtils.java
@@ -5,17 +5,31 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.DatatypeConverter;
+import me.tomi.rosetta.Strings.StringUtils;
 
 public final class HashUtils {
 
   private static final Logger log = Logger.getAnonymousLogger();
 
-  public static Optional<MessageDigest> getDigestInstance(MessageDigestType type) {
+  private static Optional<MessageDigest> getDigestInstance(MessageDigestType type) {
     try {
       return Optional.of(MessageDigest.getInstance(type.getType()));
     } catch (NoSuchAlgorithmException e) {
       log.log(Level.SEVERE, e, () -> "Cannot create the algorithm");
       return Optional.empty();
     }
+  }
+
+  public String hash(MessageDigestType type, String s) {
+    Optional<MessageDigest> instanceOpt = getDigestInstance(type);
+
+    if (instanceOpt.isPresent()) {
+      MessageDigest md = instanceOpt.get();
+      md.update(s.getBytes());
+      return DatatypeConverter.printHexBinary(md.digest());
+    }
+    
+    return StringUtils.EMPTY;
   }
 }
